66cd541446ee7b14706cba67362d3eddad748a4c
shrikantmergu143 <shriakntmergu143@gmail.com>
2023-03-14 22:12:07 +0530

    s

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ root.render(
   </React.Fragment>
 );
 
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
