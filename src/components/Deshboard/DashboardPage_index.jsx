861503e881b72586fe7e6659a6b0e084adcb6602
shrikantmergu143 <shriakntmergu143@gmail.com>
2023-03-26 17:12:12 +0530

    dasas

--- src/components/Deshboard/DashboardPage_index.jsx
+++ src/components/Deshboard/DashboardPage_index.jsx
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import shrikant_profile from "./../../assets/img/shrikant_profile.jpg"
+import shrikant_profile from "./../../assets/img/shrikant_profile.jpg";
+import IJWork from "./../../assets/img/IJWork.png";
+import Conficar from "./../../assets/img/Conficar.png";
+import Girne from "./../../assets/img/Girne.png";
+import Chathub from "./../../assets/img/Chathub.jpg";
+import Fellame from "./../../assets/img/Fellame.png";
+import ChatApp from "./../../assets/img/ChatApp1.png";
+import instashare from "./../../assets/img/instashare.png";
 import logoDefault from "./../../template/media/logo-default.png";
 import logoSticky from "./../../template/media/logo-sticky.png";
 import portfolio1 from "./../../template/media/portfolio/portfolio1.jpg"
@@ -72,6 +79,49 @@ export const Skills = [
         title:"Git",
         percentage:80,
     }
+];
+export const Works = [
+    {
+        image:Conficar,
+        title:"ConfyCar",
+        description:"By developing a car service package and admin project using React, create a modern and user-friendly interface that helps service providers manage their service packages and booking process more efficiently. The application can be customized to suit the specific needs of the service provider, making it a valuable tool for improving productivity and customer satisfaction.",
+        link:"https://bc.appristine.in"
+    },
+    {
+        image:Girne,
+        title:"Girne",
+        description:"We have developed Girne, an online marketplace in Turkey, with an aim to build a bridge between buyers and sellers through the virtual medium. People have become well-accustomed to online shopping nowadays, and the success of the online business world has inspired us to bring a platform that commits benefits for both buyers and sellers.",
+        link:"https://girne.com"
+    },
+    {
+        image:IJWork,
+        title:"IJ-Work",
+        description:"A project that includes chat, project management, to-do list, and cloud management features can be developed as a web application using React as the front-end library. In this project I worked on projects creation and todo list management.",
+        link:"https://ijwork.com"
+    },
+    {
+        image:instashare,
+        title:"InstaShare",
+        description:"InstaShare is my college major project which is submited to college. A project InstaShare is reference of Instagram.com and developing project using React Native and Native Application, create a modern and user-friendly interface that mimics the functionalities of the popular social media platform. The application can be customized to suit your specific needs, making it a valuable tool for learning React and Firebase.",
+        link:"https://instashare-7eb98.web.app"
+    },
+    {
+        image:ChatApp,
+        title:"Nation Wide",
+        description:"The Nation Wide Chat Application web are an extension of your smartphone. They mirror all the conversations and messages you have on the Nation Wide Chat app on your phone and allow you to see them all and reply to them all on your browsers using your computer's keyboard instead of the keyboard on your smartphone. In this project we have managing real time messages and notification and storing user data in redux store.",
+        link:"https://nwn.appristine.in"
+    },
+    {
+        image:Chathub,
+        title:"Chathub",
+        description:"Chathub is a real-time chat application that allows users to communicate with each other instantly. As a React developer, develop a Chathub application using React as the front-end library. Here are some of the features include in the project like Add friend and Chat with friends, My Account, Friends Profile.",
+    },
+    {
+        image:Fellame,
+        title:"Fellame",
+        description:"Fellame is a online shopping web application and developing project by using react. In this project we provides various printed fabric T`Shirt with different colors and sizes. We developed front end application and by using API`s we make a user friendly application. In this application we provide online payment on order products and coupon to users, so users can make order instantally. We have OTP base user login in application.",
+        link:"http://fme.fellame.com"
+    },
 ]
 export default function DashboardPage() {
         const [state, setState] = useState(false);
@@ -188,7 +238,7 @@ export default function DashboardPage() {
                     <div className="col-lg-8 col-12">
                         <div className="fxt-about-box-layout2">
                             <h3 className="item-title">Shrikant Mergu</h3>
-                            <div className="item-subtitle">React Developer &amp; Web Developer</div>
+                            <div className="item-subtitle">React Developer</div>
                             <p>Hi Everyone, I am <span className={"text-primary"}>Shrikant Mergu</span> from Solapur, India. I am a working as a React Developer and complete a graduation in Bachelor's Computer Application from HNCC Solapur.</p>
                             <div className="personal-info">
                                 <div className="row">
@@ -379,104 +429,31 @@ export default function DashboardPage() {
             </div>
         </section> 
 
-        <section className="fxt-service-section bg-color-1" id="section4">
+        <section className="fxt-service-section bg-color-1 my_works" id="section4">
             <div className="container">
                 <div className="fxt-heading-layout1">
-                    <h2 className="heading-title">What I do</h2>
-                    <div className="heading-paragraph">There are many variations of passages of Lorem Ipsum available</div>
+                    <h2 className="heading-title">My Recent Projects</h2>
+                    {/* <div className="heading-paragraph">There are many variations of react developer</div> */}
                     <div className="heading-line"></div>
                 </div>
                 <div className="row">
-                    <div className="col-lg-4 col-md-6 col-sm-6 col-12 mockups has-animation active-animation">
-                        <div className="translate-left-10 opacity-animation transition-200 transition-delay-100">
-                            <div className="fxt-service-box-layout1">
-                                <div className="item-img animation-delay0">
-                                    <img src="./template/media/service/service1.png" alt="service"/>
-                                </div>
-                                <div className="item-content">
-                                    <h3 className="item-title"><Link>UI/UX Design</Link ></h3>
-                                    <p>There are many variations passages of Lorem Ipsum available majority have dummy
-                                        passage.</p>
-                                    <Link className="item-btn">Read More</Link >
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6 col-sm-6 col-12 mockups has-animation active-animation">
-                        <div className="translate-left-10 opacity-animation transition-200 transition-delay-400">
-                            <div className="fxt-service-box-layout1">
-                                <div className="item-img animation-delay1">
-                                    <img src="./template/media/service/service2.png" alt="service"/>
-                                </div>
-                                <div className="item-content">
-                                    <h3 className="item-title"><Link>Web Development</Link ></h3>
-                                    <p>There are many variations passages of Lorem Ipsum available majority have dummy
-                                        passage.</p>
-                                    <Link className="item-btn">Read More</Link >
+                    {Works?.map((item, index)=>(
+                        <div key={index} className="col-lg-4 col-md-6 col-sm-6 col-12 mockups has-animation active-animation">
+                            <div className="translate-left-10 opacity-animation transition-200 transition-delay-100">
+                                <div className="fxt-service-box-layout1">
+                                    <div className="item-img animation-delay0">
+                                        <img src={item?.image} alt="service"/>
+                                    </div>
+                                    <div className="item-content">
+                                        <h3 className="item-title"><Link>{item?.title}</Link ></h3>
+                                        <p>
+                                            {item?.description}
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6 col-sm-6 col-12 mockups has-animation active-animation">
-                        <div className="translate-left-10 opacity-animation transition-200 transition-delay-700">
-                            <div className="fxt-service-box-layout1">
-                                <div className="item-img animation-delay2">
-                                    <img src="./template/media/service/service3.png" alt="service"/>
-                                </div>
-                                <div className="item-content">
-                                    <h3 className="item-title"><Link>Apps Development</Link ></h3>
-                                    <p>There are many variations passages of Lorem Ipsum available majority have dummy
-                                        passage.</p>
-                                    <Link className="item-btn">Read More</Link >
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6 col-sm-6 col-12 mockups has-animation active-animation">
-                        <div className="translate-left-10 opacity-animation transition-200 transition-delay-100">
-                            <div className="fxt-service-box-layout1">
-                                <div className="item-img animation-delay3">
-                                    <img src="./template/media/service/service4.png" alt="service"/>
-                                </div>
-                                <div className="item-content">
-                                    <h3 className="item-title"><Link>Strategy Solutions</Link ></h3>
-                                    <p>There are many variations passages of Lorem Ipsum available majority have dummy
-                                        passage.</p>
-                                    <Link className="item-btn">Read More</Link >
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6 col-sm-6 col-12 mockups has-animation active-animation">
-                        <div className="translate-left-10 opacity-animation transition-200 transition-delay-300">
-                            <div className="fxt-service-box-layout1">
-                                <div className="item-img animation-delay4">
-                                    <img src="./template/media/service/service5.png" alt="service"/>
-                                </div>
-                                <div className="item-content">
-                                    <h3 className="item-title"><Link>Digital Marketing</Link ></h3>
-                                    <p>There are many variations passages of Lorem Ipsum available majority have dummy
-                                        passage.</p>
-                                    <Link className="item-btn">Read More</Link >
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6 col-sm-6 col-12 mockups has-animation active-animation">
-                        <div className="translate-left-10 opacity-animation transition-200 transition-delay-700">
-                            <div className="fxt-service-box-layout1">
-                                <div className="item-img animation-delay5">
-                                    <img src="./template/media/service/service6.png" alt="service"/>
-                                </div>
-                                <div className="item-content">
-                                    <h3 className="item-title"><Link>Social Media</Link ></h3>
-                                    <p>There are many variations passages of Lorem Ipsum available majority have dummy
-                                        passage.</p>
-                                    <Link className="item-btn">Read More</Link >
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </section>
